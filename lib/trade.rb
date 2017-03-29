e76f61b13212aea64f3bc3a432949d4b8643239e
iantheninja <munene.ian@live.com>
2017-03-29 10:16:19 +0800

    removed commented out code from the solutions

--- lib/trade.rb
+++ lib/trade.rb
@@ -25,26 +25,4 @@ class Trade
     return {:total_KSH => total_sales.round(2), :largest => max}
 
   end
-
-
-  # define_singleton_method(:domestic_trade) do |itemId|
-  #   transactions = CSV.read('TRANS.csv')
-  #   total_sales = 0
-  #   total_location = {}
-  #
-  #   transactions.each do |row|
-  #     amount = row[2][0,4].to_f
-  #     location = row[0]
-  #     if row[1] == itemId
-  #       total_sales += amount
-  #       if !total_location[location]
-  #         total_location[location] = amount
-  #       else
-  #         total_location[location] += amount
-  #       end
-  #     end
-  #   end
-  #   max = total_location.max_by{|key, value| value}[0]
-  #   return {:total_KSH => total_sales.round(2), :largest => max}
-  # end
 end
